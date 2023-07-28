7142065bf827ff0f83a014574776aa9ea282cc5c
AbdulAziz <Raf_0707@mail.ru>
2023-07-28 20:04:02 +0300

    code with me [Raf] PerspectiveCamera.kt

--- core/src/main/kotlin/pize/graphics/camera/PerspectiveCamera.kt
+++ core/src/main/kotlin/pize/graphics/camera/PerspectiveCamera.kt
@@ -7,23 +7,37 @@ import pize.math.vecmath.matrix.Matrix4f
 
 open class PerspectiveCamera(width: Int, height: Int, near: Double, far: Double, fieldOfView: Double) :
     Camera3D(width, height), Resizable {
-    private var fieldOfView: Float
+    open var fieldOfView: Float = 0.0f
+        get() = field
+        set(fieldOfView) { this.fieldOfView = field }
+
     override var near: Float = 0.0f
+        get() = field
+        set(near) { this.near = field }
+
     override var far: Float = 0.0f
+        get() = field
+        set(far) { this.far = field }
+
     override val projection: Matrix4f?
+        get() = field
+
     override val view: Matrix4f?
-    @JvmField
+        get() = field
     val imaginaryView: Matrix4f?
-    @JvmField
+        get() = field
+
     val frustum: Frustum
+        get() = field
+
     private var dirtyProjection = false
     private var imaginaryX = false
     private var imaginaryY = false
     private var imaginaryZ = false
 
     constructor(near: Double, far: Double, fieldOfView: Double) : this(
-        Pize?.width!!,
-        Pize?.height!!,
+        Pize.width,
+        Pize.height,
         near,
         far,
         fieldOfView
@@ -78,23 +92,4 @@ open class PerspectiveCamera(width: Int, height: Int, near: Double, far: Double,
             dirtyProjection = true
         }
 
-    fun getNear(): Float {
-        return near
-    }
-
-    fun setNear(near: Float) {
-        if (this.near == near) return
-        this.near = near
-        dirtyProjection = true
-    }
-
-    fun getFar(): Float {
-        return far
-    }
-
-    fun setFar(far: Float) {
-        if (this.far == far) return
-        this.far = far
-        dirtyProjection = true
-    }
 }
